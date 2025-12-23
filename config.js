20b1b2645895237635fad5c8ee7aa9d067200cc8
Kai Zhang <hi@kaizhang.dev>
2025-12-23 20:18:14 +0100

    Add a few sites

--- config.js
+++ config.js
@@ -1,47 +1,65 @@
-export const handle = '@bearzk'
-export const handleUrl = 'https://mastodon.social/@bearzk'
+export const handle = "@bearzk";
+export const handleUrl = "https://mastodon.social/@bearzk";
 
 export const data = [
   {
-    url: 'https://hckrnews.com/',
-    name: 'hacker news',
-    keyword: 'hn',
-    category: ['dev', 'news',]
+    url: "https://hckrnews.com/",
+    name: "hacker news",
+    keyword: "hn",
+    category: ["dev", "news"],
   },
   {
-    url: 'https://lobste.rs/',
-    name: 'lobster.rs',
-    keyword: 'l',
-    category: ['dev', 'news',]
+    url: "https://lobste.rs/",
+    name: "lobster.rs",
+    keyword: "l",
+    category: ["dev", "news"],
   },
   {
-    url: 'https://theburningmonk.com/',
-    name: 'burning monk',
-    keyword: 'bm',
-    category: ['dev', 'serverless',]
+    url: "https://theburningmonk.com/",
+    name: "burning monk",
+    keyword: "bm",
+    category: ["dev", "serverless"],
   },
   {
-    url: 'https://offbynone.io/',
-    name: 'off by none',
-    keyword: 'obn',
-    category: ['dev', 'serverless',]
+    url: "https://aosabook.org/en/index.html",
+    name: "The Architecture of Open Source Applications",
+    keyword: "aosa",
+    category: ["book", "architecture"],
   },
   {
-    url: 'https://weread.qq.com/',
-    name: 'we read',
-    keyword: 'wr',
-    category: ['read',]
+    url: "https://lucumr.pocoo.org/",
+    name: "Armin Ronacher's Thoughts and Writings",
+    keyword: "ronacher",
+    category: ["dev", "llm"],
   },
   {
-    url: 'https://libbyapp.com/',
-    name: 'libby',
-    keyword: 'lb',
-    category: ['read',]
+    url: "https://eblog.fly.dev/index.html",
+    name: "Software articles by efron licht",
+    keyword: "eblog",
+    category: ["dev", "llm"],
   },
   {
-    url: 'https://www.fool.com/',
-    name: 'fool',
-    keyword: 'fool',
-    category: ['invest', 'news', ]
-  }
-]
+    url: "https://offbynone.io/",
+    name: "off by none",
+    keyword: "obn",
+    category: ["dev", "serverless"],
+  },
+  {
+    url: "https://weread.qq.com/",
+    name: "we read",
+    keyword: "wr",
+    category: ["read"],
+  },
+  {
+    url: "https://libbyapp.com/",
+    name: "libby",
+    keyword: "lb",
+    category: ["read"],
+  },
+  {
+    url: "https://www.fool.com/",
+    name: "fool",
+    keyword: "fool",
+    category: ["invest", "news"],
+  },
+];
